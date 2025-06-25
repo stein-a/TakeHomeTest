2eebaefe1c35e8b069eab197d98af02d9ab31a76
stein-a <stein.alver@me.com>
2025-06-25 11:30:36 +0200

    Initial Commit

---
+++ TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by Stein Alver on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
